9dd5c2f2984466a1d475866ece6d1ccf7a6f1042
Andrea Galindo <al.galindo@uniandes.edu.co>
2023-10-24 08:41:55 -0500

    Taller Angular VF

--- src/app/serie/serie.ts
+++ src/app/serie/serie.ts
@@ -3,18 +3,18 @@ export class Serie {
     name: string;
     channel: string;
     seasons: number;
-    linkWebPage: string;
-    linkImage: string;
+    webpage: string;
+    poster: string;
     description: string;
 
-    constructor(id: number, name: string, seasons: number, channel: string, description: string, linkWebPage: string, linkImage: string) {
+    constructor(id: number, name: string, seasons: number, channel: string, description: string, webpage: string, poster: string) {
         this.id = id;
         this.name = name;
         this.seasons = seasons;
         this.channel = channel;
         this.description = description;
-        this.linkWebPage = linkWebPage;
-        this.linkImage = linkImage;
+        this.webpage = webpage;
+        this.poster = poster;
     }
 
 }
